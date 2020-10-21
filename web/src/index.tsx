82039dfc54b007435287f4071977eb395958088a
Luciano Pierdona <lucianompj1@gmail.com>
2020-10-21 17:14:45 -0300

    added screenshot and added global styling

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
+import GlobalStyles from "./globalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
